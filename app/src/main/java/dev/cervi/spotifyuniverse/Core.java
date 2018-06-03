af88a7955788c9248fe55d7125c1b021d284ef01
Sergio Cervilla <cervick13@gmail.com>
2018-06-03 11:09:06 +0200

    Creating project structure

---
+++ app/src/main/java/dev/cervi/spotifyuniverse/Core.java
@@ -0,0 +1,13 @@
+package dev.cervi.spotifyuniverse;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Core extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_core);
+    }
+}
